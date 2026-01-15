b8b900e2c97d32efa84518b998d9de4ce5af1daf
Parbat <parbatkhatri1215@gmail.com>
2026-01-15 09:03:36 +0545

    Authorization

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -8,6 +8,8 @@ import APIFunctionality from "../utils/apiFunctionality.js";
 
 // 1 Creating Products
 export const createProducts = handleAsyncError(async (req, res, next) => {
+    req.body.user = req.user.id;
+    
     const product = await Product.create(req.body)
     res.status(201).json({
         success: true,
