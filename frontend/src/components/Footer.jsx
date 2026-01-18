71a6e81b6126952fce31f059ec25c0ec3d1aa7b1
Parbat <parbatkhatri1215@gmail.com>
2026-01-18 06:26:30 +0545

    fornt

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Phone, Mail, GitHub, Instagram, YouTube, LinkedIn } from '@mui/icons-material'
+import '../componentStyles/Footer.css'
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                {/* Section1 */}
+                <div className="footer-section contact">
+                    <h3>Contact Us</h3>
+                    <p><Phone fontSize='small' />Phone: 9821573456</p>
+                    <p><Mail fontSize='small' />Email: parbat9821@gmail.com</p>
+                </div>
+        
+                {/* Section2 */}
+                <div className="footer-sectin social">
+                    <h3>Follow Us</h3>
+                    <div className="social-links">
+                        <a href="" target='_blank'>
+                            <GitHub className='social-icon' />
+                        </a>
+                        <a href="" target='_blank'>
+                            <LinkedIn className='social-icon' />
+                        </a>
+                        <a href="" target='_blank'>
+                            <YouTube className='social-icon' />
+                        </a>
+                        <a href="" target='_blank'>
+                            <Instagram className='social-icon' />
+                        </a>
+                    </div>
+                </div>
+                <br /><br />
+                {/* Section3 */}
+                
+                <div className="footer-section about">
+                    <h3>About </h3>
+                    <p>Genuine Clothing Brands and High Quality Products.</p>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <p>&copy; 2026 All Rights Reserved</p>
+            </div>
+        </footer>
+    )
+}
+
+
+export default Footer;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../pageStyles/Home.css'
+import Footer from '../components/Footer'
+
+function Home() {
+    return (
+    <>
+        <div className="home-container">
+            <h2 className="home-heading">Trending Now</h2>
+        </div>
+        <Footer />
+        </>
+    )
+}
+
+export default Home;
